57c720f757b50735b773e31f0ad05543941ad139
Aliaksei Plashchanski <aleksey.ploschanskiy@gmail.com>
2020-06-15 18:03:30 +0300

    Masternode address check verify only address is reachable not routable, to allow local networks

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -321,8 +321,7 @@ bool CMasternode::IsValidNetAddr()
 {
     // TODO: regtest is fine with any addresses for now,
     // should probably be a bit smarter if one day we start to implement tests for this
-    return Params().NetworkID() == CBaseChainParams::REGTEST ||
-           (IsReachable(addr) && addr.IsRoutable());
+    return Params().NetworkID() == CBaseChainParams::REGTEST || IsReachable(addr);
 }
 
 CMasternodeBroadcast::CMasternodeBroadcast()
